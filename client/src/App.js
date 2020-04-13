008f53b497f83cec45d67997f050f5cfdefaa461
David <didicp4@gmail.com>
2020-04-13 19:25:06 +0100

    Setup React and SUI React
    Closes: #3

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+       <Container>
+        
+       </Container>
+    </div>
+  );
+}
+
+export default App;
