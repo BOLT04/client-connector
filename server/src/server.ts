1aa39d549331a4850181ab8a78676557c853ac08
David <didicp4@gmail.com>
2020-05-24 20:26:53 +0100

    fix(server): Shutdown after 5s for a SIGTERM
    Issue: #10

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,6 @@ import path from 'path'
 import { settings } from './settings'
 import { sendSmsHandler } from './api/send-sms.handler'
 import { verifyPhoneNumberHandler } from './api/verify-phone-number.handler'
-import { createTokenHandler } from './api/voice/create-token.handler'
 
 const app = express()
 app.set('x-powered-by', false)
@@ -19,9 +18,29 @@ app.get('/check/:number', verifyPhoneNumberHandler)
 
 const { port, isProd } = settings
 if (isProd) {
+  // Serve SPA
   const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build')
   app.use(express.static(CLIENT_BUILD_PATH))
   app.get('/*', (_, res) => res.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html')))
+
+  // Gracefully shutdown
+  process
+    .on('SIGTERM', shutdown('SIGTERM'))
+    .on('SIGINT', shutdown('SIGINT'))
+    .on('uncaughtException', shutdown('uncaughtException'));
+}
+
+// Handle process shutdown: https://help.heroku.com/D5GK0FHU/how-can-my-node-app-gracefully-shutdown-when-receiving-sigterm
+function shutdown(signal: string): NodeJS.SignalsListener {
+  return (error: any) => {
+    console.log(`${signal}...`)
+    if (error) console.error(error.stack || error);
+
+    setTimeout(() => {
+      console.log('...waited 5s, exiting.')
+      process.exit(error ? 1 : 0)
+    }, 5000).unref()
+  }
 }
 
 app.listen(port, () => {
