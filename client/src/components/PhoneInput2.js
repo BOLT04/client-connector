d46bc5376ca254b5c0fe299867c1a6db2ce73e64
David Pereira <didicp4@gmail.com>
2022-11-18 14:50:54 +0000

    Create PhoneInput2.js

---
+++ client/src/components/PhoneInput2.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "react-phone-number-input/style.css";
+import PhoneNumberInput from "react-phone-number-input";
+
+/**
+ * @param {object} props
+ * @param {string} props.value
+ * @param {(newValue: string) => void} props.onChange - Callback called when a valid phone number is inputed by the user
+ */
+function PhoneInput({ value, onChange }) {
+  return (
+    <PhoneNumberInput
+      className="phone-input"
+      placeholder="Enter phone number"
+      value={value}
+      onChange={handleChange}
+    />
+  );
+
+  /**
+   * @param {string | undefined} value 
+   */
+  function handleChange(value) {
+    if (value) onChange(value)
+  }
+}
